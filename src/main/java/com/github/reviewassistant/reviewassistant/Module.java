6b011fa6594c9201e372763df43c5b153ad0b221
Paladox none <thomasmulhall410@yahoo.com>
2018-10-19 16:47:18 +0000

    Adapats plugin to changes done in core
    Change-Id: Ida6b40187acf04ef8811bdbffd2dc64b516aec31

--- src/main/java/com/github/reviewassistant/reviewassistant/Module.java
+++ src/main/java/com/github/reviewassistant/reviewassistant/Module.java
@@ -1,10 +1,10 @@
 package com.github.reviewassistant.reviewassistant;
 
 import com.github.reviewassistant.reviewassistant.rest.GetAdvice;
-import com.google.gerrit.common.EventListener;
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.gerrit.server.events.EventListener
 
 import static com.google.gerrit.server.change.RevisionResource.REVISION_KIND;
 
