aef062bd920db6b02ce138567d2d4ba46ab46877
Francois Ferrand <thetypz@gmail.com>
2019-01-17 17:04:25 +0100

    Display review advice in polygerrit
    Change-Id: Icb2967b2d39d7693a2bb9b885746f12ace3942a7

--- src/main/resources/static/reviewassistant.js
+++ src/main/resources/static/reviewassistant.js
@@ -1,24 +1,35 @@
 Gerrit.install(function(self) {
+
+    function get(url, callback) {
+        if (window.Polymer)
+            self.restApi().get(url).then(callback)
+        else
+            Gerrit.get(url, callback);
+    }
+
     function print (c, r) {
-        if ((c.status != 'NEW') || (c.current_revision != r.name)) {
+        if ((c.status != 'NEW') || (c.current_revision != r.commit.commit)) {
             return;
         }
-        var doc = document;
-        var change_plugins = document.getElementById('change_plugins');
-        var container = doc.createElement('div');
+
+        var container = document.createElement('div');
         container.id = "reviewAssistant";
         container.style = "padding-top: 10px;";
-        var header = doc.createElement('div');
+        var header = document.createElement('div');
         header.innerHTML = "<strong>ReviewAssistant</strong>";
         container.appendChild(header);
-        var advice = doc.createElement('div');
+        var advice = document.createElement('div');
         advice.innerHtml = "<p><img src=\"plugins/reviewassistant/static/loading.gif\"></p>";
         container.appendChild(advice);
+
+        var change_plugins = document.getElementById('change_plugins');
         change_plugins.appendChild(container);
-        var url = "changes/" + c._number + "/revisions/" + r._number + "/reviewassistant~advice";
-        Gerrit.get(url, function (r) {
+
+        var url = "/changes/" + c._number + "/revisions/" + r._number + "/reviewassistant~advice";
+        get(url, function (r) {
             advice.innerHTML = r;
         });
     }
+
     self.on('showchange', print);
 });
