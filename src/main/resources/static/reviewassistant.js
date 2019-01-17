06de1c9e033858b02c5b73bda8bdde3d4da72940
Francois Ferrand <thetypz@gmail.com>
2019-01-17 17:04:25 +0100

    Display review advice in polygerrit
    Change-Id: Icb2967b2d39d7693a2bb9b885746f12ace3942a7

--- src/main/resources/static/reviewassistant.js
+++ src/main/resources/static/reviewassistant.js
@@ -1,6 +1,6 @@
 Gerrit.install(function(self) {
     function print (c, r) {
-        if ((c.status != 'NEW') || (c.current_revision != r.name)) {
+        if ((c.status != 'NEW') || (c.current_revision != r.commit.commit)) {
             return;
         }
         var doc = document;
@@ -15,10 +15,16 @@ Gerrit.install(function(self) {
         advice.innerHtml = "<p><img src=\"plugins/reviewassistant/static/loading.gif\"></p>";
         container.appendChild(advice);
         change_plugins.appendChild(container);
-        var url = "changes/" + c._number + "/revisions/" + r._number + "/reviewassistant~advice";
-        Gerrit.get(url, function (r) {
+        var url = "/changes/" + c._number + "/revisions/" + r._number + "/reviewassistant~advice";
+        get(url, function (r) {
             advice.innerHTML = r;
         });
+    }
+    function get(url, callback) {
+        if (window.Polymer)
+            self.restApi().get(url).then(callback)
+        else
+            Gerrit.get(url, callback);
     }
     self.on('showchange', print);
 });
