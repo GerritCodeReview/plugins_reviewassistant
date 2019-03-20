3b790a164a455036f6f9eec392da395345df2d4d
David Pursehouse <dpursehouse@collab.net>
2019-03-20 11:29:51 +0000

    Extend ServletModule instead of HttpPluginModule
    As described in I3b89150d1.
    
    Change-Id: Ic91ce95719c6bccc73cc29a2139a2274f20d1939

--- src/main/java/com/github/reviewassistant/reviewassistant/HttpModule.java
+++ src/main/java/com/github/reviewassistant/reviewassistant/HttpModule.java
@@ -3,9 +3,9 @@ package com.github.reviewassistant.reviewassistant;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.webui.JavaScriptPlugin;
 import com.google.gerrit.extensions.webui.WebUiPlugin;
-import com.google.gerrit.httpd.plugins.HttpPluginModule;
+import com.google.inject.servlet.ServletModule;
 
-public class HttpModule extends HttpPluginModule {
+public class HttpModule extends ServletModule {
   @Override
   protected void configureServlets() {
     DynamicSet.bind(binder(), WebUiPlugin.class)
