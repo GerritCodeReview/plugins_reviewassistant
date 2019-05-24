798c7381b7257b5eeedb157e47674e1987884a63
Francois Ferrand <thetypz@gmail.com>
2019-05-24 16:56:11 +0200

    Use protobuf for Calculation serialization
    Change-Id: Id5bc5ffb5b1589d3c030b8e45d7e1919a08343fb

--- src/main/java/com/github/reviewassistant/reviewassistant/models/Calculation.java
+++ src/main/java/com/github/reviewassistant/reviewassistant/models/Calculation.java
@@ -18,4 +18,12 @@ public class Calculation {
     this.minutes = 0;
     this.sessions = 0;
   }
+
+  public Calculation(String commitId, int totalReviewTime, int hours, int minutes, int sessions) {
+    this.commitId = commitId;
+    this.totalReviewTime = totalReviewTime;
+    this.hours = hours;
+    this.minutes = minutes;
+    this.sessions = sessions;
+  }
 }
